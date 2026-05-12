929e304724e7045a9c754efc0ed313870e65eaaf
Jeremy_D <1418776145@qq.com>
2026-05-12 14:50:56 +0800

    完成supabase连接

---
+++ App.vue
@@ -0,0 +1,21 @@
+<script>
+import { supabase } from './supabaselib/supabase'
+export default {
+	onLaunch: function () {
+		console.log('App Launch')
+	},
+	onload: function () {
+		console.log('App Load')
+	},
+	onShow: function () {
+		console.log('App Show')
+	},
+	onHide: function () {
+		console.log('App Hide')
+	}
+}
+</script>
+
+<style>
+/*每个页面公共css */
+</style>
